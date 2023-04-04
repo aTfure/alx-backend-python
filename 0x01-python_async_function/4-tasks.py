b414fa287b7e64473ad9b7bbfae885e2a9b97e4a
aTfure <atfure@gmail.com>
2023-04-04 05:41:10 +0300

    Return the list of all the delays (float values)

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-Alter wait_n to a new function task_wait_n. Nearly identical except the new function is being called."""
+4. Tasks"""
 import asyncio
 from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-	"""Return the list of all the delays (float values).List of delay are in ascending order"""
-	delays = []
-	for i in range(n):
-		delays.append(task_wait_random(max_delay))
-	return [await delay for delay in asyncio.as_completed(delays)]
+    """Return the list of all the delays (float values)"""
+    delays = []
+    for i in range(n):
+        delays.append(task_wait_random(max_delay))
+    return [await delay for delay in asyncio.as_completed(delays)]
